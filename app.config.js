f04fa9db248cc967dcb9cf0b9287ad2ed731bec8
MrDyslexia <mhhernand@gmail.com>
2025-08-05 00:31:52 -0400

    Refactor app configuration and update dependencies; remove app.json, enhance app.config.js, and adjust TypeScript settings.

--- app.config.js
+++ app.config.js
@@ -1,20 +1,69 @@
-export default {
-  expo: {
-    name: "marejadasuv",
-    slug: "marejadasuv",
-    version: "1.0.0",
-    scheme: "marejadasuv",
-    android: {
-      package: "com.lowframes.marejadasuv",
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
-      googleMaps: {
-          apiKey: process.env.GOOGLE_MAPS_API_KEY
-        }
+import 'dotenv/config';
+
+export default ({ config }) => ({
+  ...config,
+  name: "marejadasuv",
+  slug: "marejadasuv",
+  version: "1.0.0",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.lowframes.marejadasuv",
+    requireFullScreen: true
+  },
+  android: {
+    package: "com.lowframes.marejadasuv",
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
     },
-    extra: {
-      eas: {
-        projectId: "83072883-62b4-4793-8308-2102bbb1e7bc"
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY
       }
+    },
+    // Si necesitas usesCleartextTraffic
+    usesCleartextTraffic: true,
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    [
+      "expo-build-properties",
+      {
+        android: {
+          usesCleartextTraffic: true
+        }
+      }
+    ],
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      }
+    ]
+  ],
+  experiments: {
+    typedRoutes: true
+  },
+  extra: {
+    eas: {
+      projectId: "83072883-62b4-4793-8308-2102bbb1e7bc"
+    },
+    env: {
+      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+      GOOGLE_SERVICES_JSON: process.env.GOOGLE_SERVICES_JSON
     }
   }
-};
+});
